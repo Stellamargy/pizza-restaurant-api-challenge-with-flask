29f6717449ff646142ecfd6cdb0f0071d13a75ea
stlmargy <stellahmargy0211@gmail.com>
2025-06-15 14:56:51 +0300

    Fixes circular import bug and generates migration afresh

--- server/models/restaurant_pizza.py
+++ server/models/restaurant_pizza.py
@@ -1,4 +1,6 @@
 from server.app import db
+from sqlalchemy.orm import validates  
+
 from datetime import datetime,timezone
 
 class RestaurantPizza(db.Model):
@@ -8,8 +10,8 @@ class RestaurantPizza(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     prize=db.Column(db.Integer,nullable=False)
     #relationship at database level
-    restaurant_id=db.Column(db.Integer,db.ForeignKey('restaurants.id'),nullable=False)
-    pizza_id=db.Column(db.Integer,db.ForeignKey('pizzas.id'),nullable=False)
+    restaurant_id=db.Column(db.Integer,db.ForeignKey('restaurants.id', ondelete='CASCADE'),nullable=False)
+    pizza_id=db.Column(db.Integer,db.ForeignKey('pizzas.id', ondelete='CASCADE'),nullable=False)
     created_at = db.Column(
         db.DateTime(timezone=True),
         nullable=False,
@@ -30,3 +32,12 @@ class RestaurantPizza(db.Model):
     #Improve RestaurantPizza instance Readability
     def __repr__(self):
         return f"""ID:{self.id},PRIZE:{self.prize},RESTAURANT_ID:{self.restaurant_id},PIZZA_ID:{self.pizza_id},CREATED_AT:{self.created_at}"""
+    
+    #Checks for price
+    @validates('price')
+    def validate_price(self, key, value):
+        if not isinstance(value, int):
+            raise ValueError("Price must be an integer.")
+        if value < 1 or value > 10:
+            raise ValueError("Price must be between 1 and 10.")
+        return value
